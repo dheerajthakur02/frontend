344dfe71cfba710e33e517eee77a50d4168c335f
Dheeraj Kumar Thalur <114847997+dheerajthakur02@users.noreply.github.com>
2024-09-19 15:54:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://server-p56v460c4-dheerajthakur02s-projects.vercel.app/', // Your backend URL
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
 })
