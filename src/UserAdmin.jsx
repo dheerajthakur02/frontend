e650e04ed439d9c2f1dec88375a5c1f5ead69976
Dheeraj Kumar Thalur <114847997+dheerajthakur02@users.noreply.github.com>
2024-09-19 18:00:15 +0530

    Update UserAdmin.jsx

--- src/UserAdmin.jsx
+++ src/UserAdmin.jsx
@@ -3,13 +3,21 @@ import React, { useEffect, useState } from "react";
 
 const UserAdmin = () => {
   const [users, setUsers] = useState([]);
+
   useEffect(() => {
     fetchUsers();
   }, []);
 
   const fetchUsers = async () => {
     try {
-      const response = await fetch("https://server-sable-one.vercel.app/users");
+      const response = await fetch("https://server-sable-one.vercel.app/users", {
+        mode: 'cors',
+      });
+      
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
       const data = await response.json();
       setUsers(data);
     } catch (error) {
